56955d47f3c7f39ccdab903ae412c03b41cfd2dc
Guilherme Almeida <89315545+VegaCenturion@users.noreply.github.com>
2022-06-22 14:23:26 -0300

    Create avl4.c

---
+++ avl4.c
@@ -0,0 +1,23 @@
+// checa se a letra p foi digitada pelo teclado
+#include <stdio.h>
+int main()
+{
+    
+    char letras[100]; //define o tamanoho do array de char letras
+    printf("Digite algumas letras: \n"); // printa o que se é pedido
+    gets(letras); // recebe as letras digitadas 
+    int conteudo = 0; // conta as letras 'P'
+    
+    for (int i = 0; letras[i] != '\0'; i = i+1){ //incremento do vetor letras
+       if(letras[i] == 'P' || letras[i] == 'p'){ // se letras 'P's forem digitadeas aumenta 'conteudo'
+           conteudo ++; //incrementador
+        }
+    }
+    if(conteudo == 0){ //condição se p foi digitado
+        printf("Ainda bem que você não digitou a letra proibida. ");
+    } else { //condção se p não foi digitado
+        printf("Aqui odiamos a letra P de Palmeiras.");
+    }
+    return 0; // fim do programa
+}
+
